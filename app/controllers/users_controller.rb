6f28b4057592d292fa7d78a86bca0b9153ff298e
musicprogram <juan-o-henao@hotmail.es>
2020-07-15 06:56:38 -0500

    serializer no password, agrregar metodo current controlador user y routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,10 @@ class UsersController < ApplicationController
     @user.destroy
   end
 
+  def current
+    render json: current_user
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
